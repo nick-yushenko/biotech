f1d84ca588103b25d2dff6418cd9e3edaba8ade1
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2021-08-31 14:15:56 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -9,3 +9,34 @@ if (menu && burger) {
     burger.classList.toggle('active')
   })
 }
+
+
+let articleSlideOffset = (window.innerWidth -  window.innerWidth * 0.92) / 2
+console.log(articleSlideOffset)
+const articleSlider = new Swiper('.article-slider', {
+  speed: 400,
+  direction: 'horizontal',
+  slidesPerView: "auto",
+  slidesOffsetBefore: articleSlideOffset,
+  slidesOffsetAfter: articleSlideOffset,
+  spaceBetween: 15,
+
+  pagination: {
+    el: '.article-pagination',
+  },
+
+});
+
+
+// profileSlider.slides.forEach(function (slide) {
+//   slide.addEventListener('click', function (e) {
+//     let modalProfile = document.querySelector('.modal-profile')
+//     if (modalProfile) {
+//       modalProfile.classList.add('active')
+//       document.querySelector('body').style.overflow = 'hidden'
+//       modalProfileSlider.init()
+//       modalProfileSlider.slideTo(profileSlider.clickedIndex)
+
+//     }
+//   })
+// })
